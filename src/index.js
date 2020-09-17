86add93d764fe51e378b7811a6b0a72aa7c9a40c
jeffchong <jeffchong922@outlook.com>
2020-09-17 19:16:50 +0800

    添加控制路由的中间件

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
+const path = require('path')
 const express = require('express')
+const history = require('connect-history-api-fallback')
 const { SERVER_PORT } = require('./_helpers/secret')
 
 const app = express()
 
-app.get('*', (req, res) => {
-  res.end('Hello World')
+const resolve = file => path.resolve(__dirname, file)
+
+// https://github.com/bripkens/connect-history-api-fallback#introduction 查看详情
+const historyModeMiddleware = history({
+  verbose: true,
+  disableDotRule: true
+})
+
+const staticFileMiddleware = express.static(resolve('../public'))
+
+// https://github.com/bripkens/connect-history-api-fallback/tree/master/examples/static-files-and-index-rewrite#configuring-the-middleware
+app.use(staticFileMiddleware)
+app.use(historyModeMiddleware)
+app.use(staticFileMiddleware)
+// app.use('/', express.static(resolve('../public')))
+
+app.get('/', function (req, res) {
+  res.render(resolve('../public/index.html'))
 })
 
 app.listen(SERVER_PORT, () => {
